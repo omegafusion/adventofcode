c95b311c376d1142671ba8cc31037ed2cc778a34
omegafusion <39763713+omegafusion@users.noreply.github.com>
2024-12-03 01:31:26 +0000

    made a little better

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -19,24 +19,13 @@ fn read_file_contents(file_path: &String) -> (Vec<i32>, Vec<i32>) {
     (left, right)
 }
 
-fn get_lists() -> (Vec<i32>, Vec<i32>) {
+fn get_number_lists() -> (Vec<i32>, Vec<i32>) {
     println!("Enter the file path:");
     let mut file_path = String::new();
     io::stdin()
         .read_line(&mut file_path)
         .expect("Failed to read line");
-
-    let (left, right) = read_file_contents(&file_path);
-
-
-
-    // let first = get_number_list();
-    // println!("Enter the second list (space-separated)");
-    // let second = get_number_list();
-    // if first.len() != second.len() {
-    //     panic!("Different length lists");
-    // }
-    (left, right)
+    read_file_contents(&file_path)
 }
 
 fn compute_distance(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
@@ -49,91 +38,62 @@ fn compute_distance(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
     total
 }
 
+fn compute_occurrences(vector: &Vec<i32>) -> Vec<(i32, usize)> {
+    let mut sorted = vector.clone();
+    sorted.sort();
+    let mut paired: Vec<(i32, usize)> = Vec::new();
+    let mut i = 0;
+    let mut j = 0;
+    while j < vector.len() {
+        if sorted[i] == sorted[j] {
+            j += 1;
+        }
+        else {
+            let val = sorted[i];
+            let count = j - i;
+            paired.push((val, count));
+            i = j
+        }
+    }
+    if i < vector.len() {
+        let val = sorted[i];
+        let count = j - i;
+        paired.push((val, count));
+    }
+    paired
+}
+
 fn compute_similarity(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
-    let mut left_sorted = left.clone();
-    left_sorted.sort();
     let mut right_sorted = right.clone();
     right_sorted.sort();
+
+    let left_paired = compute_occurrences(left);
+    let right_paired = compute_occurrences(right);
+
     let mut total = 0;
-    let mut l0 = 0;
-    let mut l1 = 0;
-    let mut r0 = 0;
-    let mut r1 = 0;
-    while l1 < left.len() && r1 < right.len() {
-        if left_sorted[l0] == left_sorted[l1] {
-            l1 += 1;
+    let mut l = 0;
+    let mut r = 0;
+
+    while l < left_paired.len() && r < right_paired.len() {
+        let (left_val, left_occurrences) = left_paired[l];
+        let (right_val, right_occurrences) = right_paired[r];
+        if left_val < right_val {
+            l += 1;
         }
-        else if right_sorted[r0] == right_sorted[r1] {
-            r1 += 1;
-        }
-        else {
-            if left_sorted[l0] < right_sorted[r0] {
-                l0 = l1;
-            }
-            else if left_sorted[l0] > right_sorted[r0] {
-                r0 = r1;
-            }
-            else {
-                let value = left_sorted[l0];
-                let left_occurrences = l1 - l0;
-                let right_occurrences = r1 - r0;
-                total += value * (left_occurrences as i32 * right_occurrences as i32);
-                l0 = l1;
-                r0 = r1;
-            }
-        }
-    }
-    while l0 < left.len() && r1 < right.len() {
-        // Left has reached the end, but right has not.
-        // Treat the same as left reaching the next number.
-        if right_sorted[r0] == right_sorted[r1] {
-            r1 += 1;
+        else if left_val > right_val {
+            r += 1;
         }
         else {
-            if left_sorted[l0] < right_sorted[r0] {
-                l0 = l1;
-            }
-            else if left_sorted[l0] > right_sorted[r0] {
-                r0 = r1;
-            }
-            else {
-                let value = left_sorted[l0];
-                let left_occurrences = l1 - l0;
-                let right_occurrences = r1 - r0;
-                total += value * (left_occurrences as i32 * right_occurrences as i32);
-                l0 = l1;
-                r0 = r1;
-            }
-        }
-    }
-    while l1 < left.len() && r0 < right.len() {
-        // Right has reached the end, but left has not.
-        // Treat the same as right reaching the next number.
-        if left_sorted[l0] == left_sorted[l1] {
-            l1 += 1;
-        }
-        else {
-            if left_sorted[l0] < right_sorted[r0] {
-                l0 = l1;
-            }
-            else if left_sorted[l0] > right_sorted[r0] {
-                r0 = r1;
-            }
-            else {
-                let value = left_sorted[l0];
-                let left_occurrences = l1 - l0;
-                let right_occurrences = r1 - r0;
-                total += value * (left_occurrences as i32 * right_occurrences as i32);
-                l0 = l1;
-                r0 = r1;
-            }
+            total += left_val * left_occurrences as i32 * right_occurrences as i32;
+            l += 1;
+            r += 1;
         }
     }
     total
 }
 
 fn main() {
-    let (list1, list2) = get_lists();
+    let (list1, list2) = get_number_lists();
     let distance = compute_distance(&list1, &list2);
     println!("Distance: {}", distance);
     let similarity = compute_similarity(&list1, &list2);
